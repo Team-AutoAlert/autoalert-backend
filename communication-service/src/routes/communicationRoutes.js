4e1cae9829e0b93dca974fdbbb8e6a612a241eb4
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2025-06-04 02:21:24 +0530

    internet calls

--- communication-service/src/routes/communicationRoutes.js
+++ communication-service/src/routes/communicationRoutes.js
@@ -10,11 +10,13 @@ router.get('/video/rooms/:roomSid', communicationController.getRoomDetails);
 
 // Voice routes
 router.post('/voice/calls', communicationController.initiateVoiceCall);
+router.post('/voice/join', communicationController.joinInternetCall);
 router.post('/voice/token', communicationController.getVoiceToken);
 router.post('/voice-twiml', communicationController.generateVoiceTwiML);
 router.get('/voice-twiml', communicationController.generateVoiceTwiML);
 
 // Callbacks
 router.post('/room-status-callback', communicationController.roomStatusCallback);
+router.post('/call-status-callback', communicationController.roomStatusCallback);
 
 module.exports = router;
